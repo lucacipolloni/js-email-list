03536aa8e152ab18a959b056b614fb307f2de8c8
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-28 12:37:37 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+const apiUri = "https://flynn.boolean.careers/exercises/api/random/mail";
+const emailListEl = document.getElementById("email-list");
+const numbersOfEmails = 10;
+const generatedEmails = [];
 
+for (let i = 0; i < numbersOfEmails; i++) {
+  axios.get(apiUri).then((response) => {
+    const email = response.data.response;
+    generatedEmails.push(email);
 
+    if (generatedEmails.length >= numbersOfEmails) {
+      let listHtml = "";
+
+      generatedEmails.forEach((email) => {
+        listHtml += `<li>${email}</li>`;
+      });
+      emailListEl.innerHTML += listHtml;
+    }
+  });
+}
